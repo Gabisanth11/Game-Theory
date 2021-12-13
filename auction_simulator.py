b420e769c51a85b76501b269a8b1bb2a2b93fe0c
Gabisanth <gabisanth11@gmail.com>
2021-12-13 23:46:52 +0000

    added caching

--- auction_simulator.py
+++ auction_simulator.py
@@ -1,20 +1,32 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Group Noether")
 
-st.write("Second-Price, Sealed-Bid Simulator")
+st.title("Second-Price, Sealed-Bid Simulator")
 
+
+st.write("How to Use:")
+
+st.write("Set a constant value for the valuation and change the bid amount to see the payoffs for each case in the table below. From this table it will be shown that bidding your truthful valuation will never be worse, and sometimes better considering the payoff, meaning that this is a weakly dominant strategy.")
+
+st.write("The metric below shows the payoff for the current parameters you have selected.")
 #Your valuation.
-V = st.sidebar.number_input("Select your valuation of the watch:",0)
+V = st.sidebar.number_input("Select your valuation of the watch:",100)
 
 #Your bid.
-b = st.sidebar.number_input("Place your bid for the watch:",0)
+b = st.sidebar.number_input("Place your bid for the watch:",100)
 
 #Rival bid.
 r = 400;
 
 st.sidebar.write("Rival Bid is £", r)
+
+
+
+@st.cache(allow_output_mutation=True)
+def get_data():
+    return []
+
 
 if b > V:
     if r < V:
@@ -31,15 +43,9 @@ if b > V:
     
     st.metric("You bid higher than your valuation and your payoff is:", Payoff_case1)
 
-elif b == V:
-    if r < b:
-        Payoff_case2 = V - r
+    get_data().append({"Bid Higher than your Valuation": Payoff_case1, "Bid Lower than your Valuation": None , "Bid your Valuation": None})
+    st.write(pd.DataFrame(get_data()))
 
-    elif r >= b:
-        Payoff_case2 = 0
-    
-    st.metric("You bid your valuation and your payoff is:", Payoff_case2)
-    
 
 elif b < V:
     if r < b:
@@ -50,4 +56,25 @@ elif b < V:
         Payoff_case3 = 0.5*(V-r)
     
     st.metric("You bid lower than your valuation and your payoff is:", Payoff_case3)
+
+    get_data().append({"Bid Higher than your Valuation": None, "Bid Lower than your Valuation": Payoff_case3 , "Bid your Valuation": None})
+    st.write(pd.DataFrame(get_data()))
+
+elif b == V:
+    if r < b:
+        Payoff_case2 = V - r
+
+    elif r >= b:
+        Payoff_case2 = 0
     
+    st.metric("You bid your valuation and your payoff is:", Payoff_case2)
+
+    get_data().append({"Bid Higher than your Valuation": None, "Bid Lower than your Valuation": None , "Bid your Valuation": Payoff_case2})
+    st.write(pd.DataFrame(get_data()))
+    
+
+
+
+if st.button('Clear Data'):
+     st.legacy_caching.clear_cache()
+     st.legacy_caching.clear_cache()
